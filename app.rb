83b4d9a8039896f73c7aca23cba4cff5d6ed315c
Srija S <srijakumar7@gmail.com>
2018-09-14 23:49:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,4 +11,8 @@ class App < Sinatra::Base
     (params[:number].to_i**2).to_s
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
 end
